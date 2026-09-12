08e4fcba90d76876b1994ea6d2e696115cb4b375
moemugi <whackeenfranco@gmail.com>
2026-09-12 21:09:27 +0800

    [WIP] cors middleware for vercel

--- api/main.py
+++ api/main.py
@@ -40,7 +40,8 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://pusakal.vercel.app"
     ],
 
     allow_credentials=True,
